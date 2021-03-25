4e8c60a507da2fbf8e883c628943bb5bfed7c309
jmdaugherty94@gmail.com <jmdaugherty94@gmail>
2021-03-25 09:55:03 -0400

    latest add

---
+++ js/nameshw.js
@@ -0,0 +1,27 @@
+let friends = [];
+
+const addFriend = () => {
+    let fname = document.getElementById("newname").value;
+    let femail = document.getElementById("email").value;
+    let fphone = document.getElementById("phone").value;
+    let friend = {
+        name: fname,
+        email: femail,
+        phone: fphone 
+    }
+    friends.push(friend);
+    display();
+}
+
+const display = () => {
+    let tbody = document.getElementById("tbody");
+    tbody.innerHTML = "";
+    for(let friend of friends){
+        let tr = "<tr>";
+        tr += `<td>${friend.name}</td>`;
+        tr += `<td>${friend.email}</td>`;
+        tr += `<td>${friend.phone}</td>`;
+        tr += "</tr>";
+        tbody.innerHTML += tr;
+    }
+}
